1aa1c1532005f5282e5bc898e7f6f3ea39ae1881
Ivan Gutierrrez <macintario@gmail.com>
2025-04-02 14:55:30 -0600

    mejorar la detección en raspberry

--- watch.py
+++ watch.py
@@ -1,3 +1,5 @@
+import time
+
 import cv2
 #import gradio as gr
 #import numpy as np
@@ -15,30 +17,33 @@ if not cap.isOpened():
     print("Error opening video file")
 while cap.isOpened():
     # Capture frame-by-frame
-      ret, frame = cap.read()
+      time.sleep(.2)
+      ret, frame = cap.read(cv2.IMREAD_REDUCED_GRAYSCALE_4)
       cv2.waitKey(1)
       if ret:
         # Apply background subtraction
         if i==0:
             fg_mask = backSub.apply(frame)
         # Find contours
-        contours, hierarchy = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#        contours, hierarchy = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv2.findContours(fg_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         # print(contours)
-        frame_ct = cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
+        frame_ct = cv2.drawContours(frame, contours, -1, (100, 0, 0), 2)
         # Display the resulting frame
         cv2.imshow('Frame_final', frame_ct)
         # apply global threshold to remove shadows
-        retval, mask_thresh = cv2.threshold(fg_mask, 220, 255, cv2.THRESH_BINARY)
+#        retval, mask_thresh = cv2.threshold(fg_mask, 500, 255, cv2.THRESH_BINARY)
+        retval, mask_thresh = cv2.threshold(fg_mask, 500, 255, cv2.THRESH_TOZERO_INV)
         # set the kernal
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4, 4))
         # Apply erosion
         mask_eroded = cv2.morphologyEx(mask_thresh, cv2.MORPH_OPEN, kernel)
-        min_contour_area = 2000  # Define your minimum area threshold
+        min_contour_area = 1500  # Define your minimum area threshold
         large_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_contour_area]
         frame_out = frame.copy()
         for cnt in large_contours:
             x, y, w, h = cv2.boundingRect(cnt)
-            frame_out = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 200), 3)
+            frame_out = cv2.rectangle(frame, (x, y), (x + w, y + h), (200, 200, 0), 3)
 
         # Display the resulting frame
         cv2.imshow('Frame_final', frame_out)
