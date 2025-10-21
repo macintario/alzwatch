bbd335ab811422efe3a15f7fa8a13b0b35fa4050
Ivan Gutierrrez <macintario@gmail.com>
2025-10-21 12:27:22 -0600

    acondicionar a ambiente uam

--- watch.sh
+++ watch.sh
@@ -1,10 +1,10 @@
 #/usr/bin/bash
 echo "Updating ..."
-cd /home/ivan_/alzwatch
+cd /home/monica/alzwatch
 git pull
 echo "Starting ..."
 /usr/bin/python --version
 sleep 2
-/usr/bin/python /home/ivan_/alzwatch/watch.py
+/usr/bin/python /home/monica/alzwatch/watch.py
 sleep 10
 pause
